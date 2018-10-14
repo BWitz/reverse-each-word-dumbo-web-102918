d44ca6e6b9338463eeb41d0acf3c39b5ba10cb69
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-14 04:35:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_reversed = sentence.split("").collect do |x|
-    x.reverse
-  end 
-  sentence_reversed.join("")
+  x.reverse
+end 
+  sentence_reversed.join(" ")
 end
