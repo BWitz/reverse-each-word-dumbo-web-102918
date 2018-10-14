78fa9bd2d9d70f8c3ea53bf1a5d6f9e519f150f8
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-14 04:02:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = "I want my code to work pls"
 
 def reverse_each_word(sentence)
-  sentence1.each do |x|
-    puts "#{reverse_each_word(x).reverse}"
+  sentence1.split do |x|
+    
   end
 end
